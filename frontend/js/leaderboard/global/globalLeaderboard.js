171f74ec71eba2cdc2c23c41074c48acc1e2e14c
Your Ambitions <artemonbagrakov@icloud.com>
2026-08-04 16:33:10 +0300

    new

--- frontend/js/leaderboard/global/globalLeaderboard.js
+++ frontend/js/leaderboard/global/globalLeaderboard.js
@@ -1,37 +1,45 @@
 import {
     renderLeaderboardSection
-} from "../leaderboardSection.js"
-
-topUsers: [
-    {
-        rank: 2,
-        name: "Мария",
-        xp: "39 600",
-        avatar: "/img/profile/avatar beginer.png"
-    },
-    {
-        rank: 1,
-        name: "Александр",
-        xp: "50 230",
-        avatar: "/img/profile/avatar beginer.png"
-    },
-    {
-        rank: 3,
-        name: "Игорь",
-        xp: "38 640",
-        avatar: "/img/profile/avatar beginer.png"
-    }
-]
+} from "../leaderboardSection.js";
 
 
 export function renderGlobalLeaderboard() {
+
     return renderLeaderboardSection({
+
         intro: {
             title: "Глобальный рейтинг",
             subtitle:
                 "Соревнуйтесь с лучшими привычками мира",
             backgroundImage:
                 "/img/leaderboard/global-section-intro.jpg"
-        }
-    })
+        },
+
+        topUsers: [
+
+            {
+                rank: 2,
+                name: "Мария",
+                xp: "39 600",
+                avatar: "/img/profile/avatar beginer.png"
+            },
+
+            {
+                rank: 1,
+                name: "Александр",
+                xp: "50 230",
+                avatar: "/img/profile/avatar beginer.png"
+            },
+
+            {
+                rank: 3,
+                name: "Игорь",
+                xp: "38 640",
+                avatar: "/img/profile/avatar beginer.png"
+            }
+
+        ]
+
+    });
+
 }
