d24bc9bbda80fd54eb0ebea7429eeea80a4a729c
Your Ambitions <artemonbagrakov@icloud.com>
2026-08-05 13:46:23 +0300

    new

--- backend/api/app.py
+++ backend/api/app.py
@@ -41,6 +41,7 @@ app.add_middleware(
     allow_origins=[
         "https://ahabit.org",
         "https://www.ahabit.org",
+        "https://ahabitbot.vercel.app",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
